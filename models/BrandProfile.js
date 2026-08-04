2e93d74ec84b47f562b0d6e6951a6c1b4a26ba51
Anupama Awadhiya <ishuawadhiya26@gmail.com>
2026-08-04 12:21:13 +0530

    just commit

--- models/BrandProfile.js
+++ models/BrandProfile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const BrandProfileSchema = new mongoose.Schema(
   {
     workspaceId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String,
       ref: 'Workspace',
       required: true,
       index: true,
